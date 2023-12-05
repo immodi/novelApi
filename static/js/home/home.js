97eec2d27af5067d623a3f6feef3e394f56a60f6
immodi <ay727467@gmail.com>
2023-12-05 23:27:14 +0200

    fuck it

--- static/js/home/home.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector("#form")
-const button = document.querySelector("#submitButton")
-
-
-button.addEventListener("click", () => {
-    button.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...'
-    button.disabled = true;
-    form.submit()
-})
