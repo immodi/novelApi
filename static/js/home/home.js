27760e765443299f1bf7f46c77bbc77b97459c2c
immodi <ay727467@gmail.com>
2023-12-06 19:58:01 +0200

    kj

--- static/js/home/home.js
+++ static/js/home/home.js
@@ -57,7 +57,7 @@ function startBar(fileSize, speedInMBps) {
     let originalSizeInMb = fileSize
 
     let interval = setInterval(() => {
-        let percentage = (100 - parseInt((fileSize / originalSizeInMb) * 100)) * 8
+        let percentage = (100 - parseFloat((fileSize / originalSizeInMb) * 100)) * 8
         progressBar.ariaValueNow = `${percentage}`
         progressBar.style.width = `${percentage}%`
         progressBar.innerHTML = `${percentage.toFixed(1)}%`
